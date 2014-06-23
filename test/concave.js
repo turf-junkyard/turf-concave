a8aa1d342cee8ad28b19548193722e6589c2d1ed
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-06-23 19:12:34 -0400

    formatting

--- test/concave.js
+++ test/concave.js
@@ -7,10 +7,10 @@ var REGEN = false;
 
 test('intersect', function(t){
   glob.sync(__dirname + '/fixtures/in/*.geojson').forEach(function(input) {
-      var fcs = JSON.parse(fs.readFileSync(input));
-      var output = concave(fcs, 2.5);
-      if (REGEN) fs.writeFileSync(input.replace('/in/', '/out/'), JSON.stringify(output));
-      t.deepEqual(output, JSON.parse(fs.readFileSync(input.replace('/in/', '/out/'))), input);
+    var fcs = JSON.parse(fs.readFileSync(input));
+    var output = concave(fcs, 2.5);
+    if (REGEN) fs.writeFileSync(input.replace('/in/', '/out/'), JSON.stringify(output));
+    t.deepEqual(output, JSON.parse(fs.readFileSync(input.replace('/in/', '/out/'))), input);
   });
   t.end();
 })
