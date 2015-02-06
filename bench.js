b5c6116d2286bdbb0e1156dbfef1350276d2896b
morganherlocker <morgan.herlocker@gmail.com>
2015-02-06 12:40:03 -0500

    refactor bench

--- bench.js
+++ bench.js
@@ -2,12 +2,16 @@ var concave = require('./');
 var Benchmark = require('benchmark');
 var fs = require('fs');
 
-var fcs = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/concaveIn2.geojson'));
+var pts1 = JSON.parse(fs.readFileSync(__dirname+'/fixtures/in/pts1.geojson'));
+var pts2 = JSON.parse(fs.readFileSync(__dirname+'/fixtures/in/pts2.geojson'));
     
 var suite = new Benchmark.Suite('turf-concave');
 suite
-  .add('turf-concave',function () {
-  	concave(fcs, 2.5, 'miles');
+  .add('turf-concave#simple',function () {
+  	concave(pts1, 2.5, 'miles');
+  })
+  .add('turf-concave#complex',function () {
+  	concave(pts2, 2.5, 'miles');
   })
   .on('cycle', function (event) {
     console.log(String(event.target));
